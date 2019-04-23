2d01f856e523a123a04d6a0bdf8c4baef0248e6a
Neil Wilson <neil@aldur.co.uk>
2019-04-23 12:17:15 +0100

    Make domain tests less brittle
    Check for a prefix rather than an entire match

--- brightbox/load_balancer_test.go
+++ brightbox/load_balancer_test.go
@@ -17,6 +17,7 @@ package brightbox
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/brightbox/gobrightbox"
@@ -741,14 +742,14 @@ func TestValidateDomains(t *testing.T) {
 				serviceAnnotationLoadBalancerSslDomains: missingDomain,
 			},
 			cloudIp: &resolvCip,
-			status:  "Failed to resolve \"" + missingDomain + "\" to load balancer address (" + resolvCip.PublicIPv4 + "," + resolvCip.PublicIPv6 + "): lookup " + missingDomain + ": no such host",
+			status:  "Failed to resolve \"" + missingDomain + "\" to load balancer address (" + resolvCip.PublicIPv4 + "," + resolvCip.PublicIPv6 + "):",
 		},
 		"missing domain in list": {
 			annotations: map[string]string{
 				serviceAnnotationLoadBalancerSslDomains: resolvedDomain + "," + missingDomain,
 			},
 			cloudIp: &resolvCip,
-			status:  "Failed to resolve \"" + missingDomain + "\" to load balancer address (" + resolvCip.PublicIPv4 + "," + resolvCip.PublicIPv6 + "): lookup " + missingDomain + ": no such host",
+			status:  "Failed to resolve \"" + missingDomain + "\" to load balancer address (" + resolvCip.PublicIPv4 + "," + resolvCip.PublicIPv6 + "):",
 		},
 		"other addresses": {
 			annotations: map[string]string{
@@ -770,7 +771,7 @@ func TestValidateDomains(t *testing.T) {
 			err := validateContextualAnnotations(tc.annotations, tc.cloudIp)
 			if err == nil {
 				t.Errorf("Expected error %q got nil", tc.status)
-			} else if err.Error() != tc.status {
+			} else if !strings.HasPrefix(err.Error(), tc.status) {
 				t.Errorf("Expected %q, got %q", tc.status, err.Error())
 			}
 		})
