5115eccaef3b78355218b5392e957322aa57958a
Neil Wilson <neil@aldur.co.uk>
2020-01-23 11:20:06 +0000

    Use OpenAPI client type annotations
    Create job with just one explicit class reference
    
    The make method dynamically creates references within the K8s Api
    object using the type annotations stored in the class definitions.
    
    This reduces errors and allows the type checking built into the Api
    to function more effectively. If you reference the wrong attribute
    you will get an expection.

--- cloud-build/run-docker-jobs.rb
+++ cloud-build/run-docker-jobs.rb
@@ -1,91 +1,100 @@
 #!/usr/bin/env ruby
 
-$LOAD_PATH.unshift File.expand_path('kubernetes-client/lib', __dir__) 
+$LOAD_PATH.unshift File.expand_path('kubernetes-client/lib', __dir__)
 
 require 'kubernetes'
 require 'active_support/core_ext/hash/keys'
-require 'open3'
+require 'active_support/core_ext/string/inflections'
 require 'yaml'
 
-first_release=14
-last_release=17
+first_release = 14
+last_release = 17
 config = {
-  prefix:"cloud-controller-build",
-  image:"gcr.io/kaniko-project/executor:latest",
-  requestsCpu:"1700m",
-  requestsMemory:"1Gi",
-  limitsCpu:"2",
-  limitsMemory:"1890Mi",
-  gitRepo:"git://github.com/brightbox/brightbox-cloud-controller-manager.git",
-  dockerTarget:"brightbox/brightbox-cloud-controller-manager",
-  secretName:"regcred",
-  holdTime:600,
+  prefix: 'cloud-controller-build',
+  image: 'gcr.io/kaniko-project/executor:latest',
+  requestsCpu: '1700m',
+  requestsMemory: '1Gi',
+  limitsCpu: '2',
+  limitsMemory: '1890Mi',
+  gitRepo: 'git://github.com/brightbox/brightbox-cloud-controller-manager.git',
+  dockerTarget: 'brightbox/brightbox-cloud-controller-manager',
+  secretName: 'regcred',
+  holdTime: 600
 }
+
+def make(target, attribute, attrs = {})
+  create_object = lambda { |class_name, attrs|
+    "Kubernetes::#{class_name}".constantize.new(attrs)
+  }
+  new_class = target.class.openapi_types[attribute.to_sym]
+  target.public_send(
+    :"#{attribute}=",
+    case new_class
+    when nil
+      raise ArgumentError, attribute
+    when /^Array<(?<class_name>\S+)>$/
+      [create_object.call(Regexp.last_match[:class_name], attrs)]
+    when /^Hash</
+      attrs
+    else
+      create_object.call(new_class, attrs)
+    end
+  )
+end
 
 def create_job_manifest(release, config)
   version = `git describe --always release-#{release}`.strip.sub(/^v([0-9]+\.[0-9]+\.[0-9]+).*$/, '\1')
-  name = "#{config[:prefix]}-#{version.gsub('.', '-')}"
-  volume_mount = Kubernetes::IoK8sApiCoreV1VolumeMount.new(
-    name: config[:secretName], mount_path: "/root"
-  )
-  volume = Kubernetes::IoK8sApiCoreV1Volume.new(
-    name: config[:secretName],
-    secret: Kubernetes::IoK8sApiCoreV1SecretVolumeSource.new(
-      secret_name: config[:secretName],
-      items: [
-        Kubernetes::IoK8sApiCoreV1KeyToPath.new(
-          key: ".dockerconfigjson", path: ".docker/config.json"
-        )
-      ]
-    )
-  )
-  container = Kubernetes::IoK8sApiCoreV1Container.new(
-    name: name,
-    image: config[:image],
-    resources: Kubernetes::IoK8sApiCoreV1ResourceRequirements.new(
-      requests: {
-        memory: config[:requestsMemory],
-        cpu: config[:requestsCpu],
-      },
-      limits: {
-        memory: config[:limitsMemory],
-        cpu: config[:limitsCpu],
-      },
-    ),
-    args: [
-      "--dockerfile=Dockerfile",
-      "--context=#{config[:gitRepo]}#refs/heads/release-#{release}",
-      "--destination=#{config[:dockerTarget]}:#{version}"
-    ],
-    volume_mounts: [volume_mount]
-  )
+  name = "#{config[:prefix]}-#{version.tr('.', '-')}"
   job = Kubernetes::IoK8sApiBatchV1Job.new(
-    api_version: "batch/v1",
-    kind: "Job",
-    metadata: Kubernetes::IoK8sApimachineryPkgApisMetaV1ObjectMeta.new(
-      name: name,
-      labels: { build: config[:prefix] }
-    ),
-    spec: Kubernetes::IoK8sApiBatchV1JobSpec.new(
-      ttl_seconds_after_finished: config[:holdTime],
-      template: Kubernetes::IoK8sApiCoreV1PodTemplateSpec.new(
-        spec: Kubernetes::IoK8sApiCoreV1PodSpec.new(
-          restart_policy: "Never",
-          containers: [container],
-          volumes: [volume],
-        )
-      )
-    ),
+    api_version: 'batch/v1',
+    kind: 'Job'
   )
+  make(job, :metadata,
+       name: name,
+       labels: { build: config[:prefix] })
+  make(job, :spec,
+       ttl_seconds_after_finished: config[:holdTime])
+  make(job.spec, :template)
+  make(job.spec.template, :spec,
+       restart_policy: 'Never')
+  spec = job.spec.template.spec
+  make(spec, :volumes,
+       name: config[:secretName])
+  volume = spec.volumes.first
+  make(volume, :secret,
+       secret_name: config[:secretName])
+  make(volume.secret, :items,
+       key: '.dockerconfigjson',
+       path: '.docker/config.json')
+  make(spec, :containers,
+       name: name,
+       image: config[:image],
+       args: [
+         '--dockerfile=Dockerfile',
+         "--context=#{config[:gitRepo]}#refs/heads/release-#{release}",
+         "--destination=#{config[:dockerTarget]}:#{version}"
+       ])
+  container = spec.containers.first
+  make(container, :resources,
+       requests: {
+         memory: config[:requestsMemory],
+         cpu: config[:requestsCpu]
+       },
+       limits: {
+         memory: config[:limitsMemory],
+         cpu: config[:limitsCpu]
+       })
+  make(container, :volume_mounts,
+       name: config[:secretName],
+       mount_path: '/root')
   job.to_hash.deep_stringify_keys.to_yaml
 end
 
-jobs=(first_release..last_release).inject("") do |memo, release|
+jobs = (first_release..last_release).inject('') do |memo, release|
   memo << create_job_manifest("1.#{release}", config)
 end
 
-puts "Running jobs on k8s"
 puts jobs
-#stdout_str, status = Open3.capture2('kubectl apply -f -', stdin_data: jobs)
-#puts stdout_str
-#exit status.exitstatus
+# stdout_str, status = Open3.capture2('kubectl apply -f -', stdin_data: jobs)
+# puts stdout_str
+# exit status.exitstatus
