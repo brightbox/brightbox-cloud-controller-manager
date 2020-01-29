01a386b8ac61e30f69fc455f94679397fea615c1
Neil Wilson <neil@aldur.co.uk>
2020-01-29 10:07:42 +0000

    Update spec to use dot form from hash form

--- cloud-build/spec/create_docker_jobs_spec.rb
+++ cloud-build/spec/create_docker_jobs_spec.rb
@@ -3,6 +3,6 @@ describe '#create_job_manifest' do
   job = create_job_manifest('1.16', config, '1.16.1', 'job-1.16')
 
   it 'has a restart Policy of never' do
-    expect(job[:spec][:template][:spec][:restartPolicy]).to eq 'Never'
+    expect(job.spec.template.spec.restart_policy).to eq 'Never'
   end
 end
