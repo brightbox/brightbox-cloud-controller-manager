31520a1c189605da2ee9181f96497b13b4622e3e
Neil Wilson <neil@aldur.co.uk>
2020-01-29 11:45:05 +0000

    Use a 'let' in the spec

--- cloud-build/spec/create_docker_jobs_spec.rb
+++ cloud-build/spec/create_docker_jobs_spec.rb
@@ -1,6 +1,8 @@
-load 'create_docker_jobs'
 describe '#create_job_manifest' do
-  job = create_job_manifest('1.16', config, '1.16.1', 'job-1.16')
+  let(:job) do
+    load 'create_docker_jobs'
+    create_job_manifest('1.16', config, '1.16.1', 'job-1.16')
+  end
 
   it 'has a restart Policy of never' do
     expect(job[:spec][:template][:spec][:restartPolicy]).to eq 'Never'
