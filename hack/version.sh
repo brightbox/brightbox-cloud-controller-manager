d705feb4c1a6867cfc88757a6435cd800883c9b1
Neil Wilson <neil@aldur.co.uk>
2020-08-28 14:59:02 +0100

    Update copyright on updated version script

--- hack/version.sh
+++ hack/version.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-# Copyright 2018 Brightbox Systems Ltd
+# Copyright 2020 Brightbox Systems Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 # -----------------------------------------------------------------------------
 # Version management helpers.  These functions help to set, save and load the
 # following variables:
