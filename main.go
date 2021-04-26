8c12bfc813a2d249cd241b29dae104f3845b886d
Neil Wilson <neil@aldur.co.uk>
2021-04-26 09:42:40 +0100

    Update main function
    - Update main to the new call process and remove
    the workaround from previous versions.
    - Update the boilerplate check to handle 2021 as a year.
    - Remove redundant main update task in Makefile.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Brightbox Systems Ltd
+// Copyright 2021 Brightbox Systems Ltd
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// The external controller manager is responsible for running controller loops that
-// are cloud provider dependent. It uses the API to listen to new events on resources.
-
 package main
 
 import (
@@ -22,22 +19,65 @@ import (
 	"os"
 	"time"
 
-	"github.com/brightbox/brightbox-cloud-controller-manager/app"
 	_ "github.com/brightbox/brightbox-cloud-controller-manager/brightbox"
+	"github.com/spf13/pflag"
+	"k8s.io/apimachinery/pkg/util/wait"
+	cloudprovider "k8s.io/cloud-provider"
+	"k8s.io/cloud-provider/app"
+	"k8s.io/cloud-provider/app/config"
+	"k8s.io/cloud-provider/options"
+	cliflag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/logs"
-
 	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
 	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
+	"k8s.io/klog/v2"
+	// For existing cloud providers, the option to import legacy providers is still available.
+	// e.g. _"k8s.io/legacy-cloud-providers/<provider>"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
+	ccmOptions, err := options.NewCloudControllerManagerOptions()
+	if err != nil {
+		klog.Fatalf("unable to initialize command options: %v", err)
+	}
+
+	fss := cliflag.NamedFlagSets{}
+	command := app.NewCloudControllerManagerCommand(ccmOptions, cloudInitializer, app.DefaultInitFuncConstructors, fss, wait.NeverStop)
+
+	// TODO: once we switch everything over to Cobra commands, we can go back to calling
+	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
+	// normalize func and add the go flag set by hand.
+	// Here is an sample
+	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
+	// utilflag.InitFlags()
 	logs.InitLogs()
 	defer logs.FlushLogs()
-
-	command := app.NewBrightboxCloudControllerManagerCommand()
 
 	if err := command.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func cloudInitializer(config *config.CompletedConfig) cloudprovider.Interface {
+	cloudConfig := config.ComponentConfig.KubeCloudShared.CloudProvider
+
+	// initialize cloud provider with the cloud provider name and config file provided
+	cloud, err := cloudprovider.InitCloudProvider(cloudConfig.Name, cloudConfig.CloudConfigFile)
+	if err != nil {
+		klog.Fatalf("Cloud provider could not be initialized: %v", err)
+	}
+	if cloud == nil {
+		klog.Fatalf("Cloud provider is nil")
+	}
+
+	if !cloud.HasClusterID() {
+		if config.ComponentConfig.KubeCloudShared.AllowUntaggedCloud {
+			klog.Warning("detected a cluster without a ClusterID.  A ClusterID will be required in the future.  Please tag your cluster to avoid any future issues")
+		} else {
+			klog.Fatalf("no ClusterID found.  A ClusterID is required for the cloud provider to function properly.  This check can be bypassed by setting the allow-untagged-cloud option")
+		}
+	}
+	return cloud
 }
